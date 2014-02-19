3d9061a14fd1b3062ba0e9a3024629004f5cb190
Dominic Bosch <dominicsfakeaddress@server.fake>
2014-02-19 06:53:37 +0100

    removed unnecessary files from staging

--- testing/old/mod_user.js
+++
@@ -1,9 +0,0 @@
-
-exports.setUp = function() {
-  this.mod = require('./user');
-};
-
-exports.addUser = function() {
-    test.ok(false, "needs implementation");
-  
-};
--- testing/old/unit_integration.js
+++
@@ -1,7 +0,0 @@
-
-exports.testUnitIntegration = function(test){
-    test.ok(false, "needs implementation");
-    test.done();
-};
-//TODO malicious module has to be loaded only for testing, the error
-// it causes need to be verified and in the end the module need to be deleted again
